673d15b550321db69f50373757f4d823d500c9b9
leeyounhun <107522866+leeyounhun@users.noreply.github.com>
2022-10-26 17:20:25 +0900

    Refactor: mail send code integrated method on service

--- src/main/java/com/kh/controller/MemberController.java
+++ src/main/java/com/kh/controller/MemberController.java
@@ -1,19 +1,13 @@
 package com.kh.controller;
 
-import java.io.Console;
-import java.security.Principal;
 import java.util.Random;
 import java.util.UUID;
-
-import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
